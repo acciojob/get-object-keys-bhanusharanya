6d1a7f01d40d39e35052363a0a3bd8019cfc6569
bhanusharanya <sharanya2630@gmail.com>
2024-07-11 11:50:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+
+student.name = "John Doe";
+let student={
+	name="sharanya"
+}
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+};
+
+
+console.log(student.getKeys()); 
